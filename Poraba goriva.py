32f58b1ca33d4e358f63811354fd9235be3a275f
Ninca98 <strovs.nina@gmail.com>
2018-07-31 23:10:38 +0200

    Dodana druga funkcija, dopisani komentarji.

--- Poraba goriva.py
+++ Poraba goriva.py
@@ -5,6 +5,7 @@ from tkinter.font import Font
 
 
 
+# Funkcija, ki pove, ali so v vnosnem polju samo števila in vejice oz. pike.
 def ali_so_samo_stevila(vnos):
     vpisan_niz = str(vnos).replace(',', '').replace('.', '')
     vpisan_seznam = list(vpisan_niz)
@@ -20,17 +21,20 @@ class GorivoModel:
     def __init__(self, vmesnik):
         self.vnos1 = vmesnik.vnos1
         self.vnos2 = vmesnik.vnos2
-        #self.vnos3 = vmesnik.vnos3
-        #self.vnos4 = vmesnik.vnos4
+        self.vnos3 = vmesnik.vnos3
+        self.vnos4 = vmesnik.vnos4
         self.izpis12 = vmesnik.izpis12
-        #self.izpis34 = vmesnik.izpis34
+        self.izpis34 = vmesnik.izpis34
 
-  
+
+ 
 
     def izracun12(self):
         izracunana_poraba = 0
 
+        # Najprej preverimo, ali so v vnosnih poljih samo števila.
         if ali_so_samo_stevila(self.vnos1.get()) == True and ali_so_samo_stevila(self.vnos2.get()) == True:
+            # Če je napisana dec. vejica, jo zamenjamo s piko.
             if ',' in self.vnos1.get() or ',' in self.vnos2.get():
                 vnos1 = self.vnos1.get().replace(',', '.')
                 vnos2 = self.vnos2.get().replace(',', '.')
@@ -54,6 +58,7 @@ class GorivoModel:
                     self.izpis12.delete(0, tk.END)
                     self.izpis12.insert(0, str(izracunana_poraba))
 
+            # Tu je napisana dec. pika, zato ni potrebno popravljati vnosa.
             else:
                 vnesena_kolicina_goriva = float(self.vnos1.get())
                 vneseni_km = float(self.vnos2.get())
@@ -74,18 +79,92 @@ class GorivoModel:
                     izracunana_poraba = round(vneseni_km / vnesena_kolicina_goriva , 4)
                     self.izpis12.delete(0, tk.END)
                     self.izpis12.insert(0, str(izracunana_poraba))
-                
+
+        # Če niso samo števila, funkcija vrne 'Neveljaven vnos'.        
         else:
             self.izpis12.delete(0, tk.END)
-            self.izpis12.insert(0, "Neveljaven vnos!")
+            self.izpis12.insert(0, 'Neveljaven vnos!')
 
 
 
+
+
+
+    def izracun34(self):
+        izracunana_cena = 0
+
+        # Najprej preverimo, če je poraba sploh izračunana.
+        if self.izpis12.get() in ['Vnesite vrednosti!', 'Vnesite količino goriva!', 'Vnesite kilometre!', 'Neveljaven vnos!']:
+            self.izpis34.delete(0, tk.END)
+            self.izpis34.insert(0, 'Izračunajte porabo!')
+
+##        # Preverimo, če je poraba slučajno 0.
+##        elif self.izpis12.get() == 0:
+##            self.izpis34.delete(0, tk.END)
+##            self.izpis34.insert(0, 'Izračunajte porabo!')
+
+        # V primeru, da je poraba izračunana, nadaljujemo s funkcijo.
+        else:
+            poraba = float(self.izpis12.get())
+            if ali_so_samo_stevila(self.vnos3.get()) == True and ali_so_samo_stevila(self.vnos4.get()) == True:
+                if ',' in self.vnos3.get() or ',' in self.vnos4.get():
+                    vnos3 = self.vnos3.get().replace(',', '.')
+                    vnos4 = self.vnos4.get().replace(',', '.')
+                    vnesena_cena_goriva = float(vnos3)
+                    vneseni_nacrtovani_km = float(vnos4)
+                
+            
+                    if vnesena_cena_goriva == 0 and vneseni_nacrtovani_km == 0:
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0,'Vnesite vrednosti!')
+                        
+                    elif vnesena_cena_goriva == 0 and vneseni_nacrtovani_km != 0:
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0, 'Vnesite ceno goriva!')
+                        
+                    elif vnesena_cena_goriva != 0 and vneseni_nacrtovani_km == 0:
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0, 'Vnesite nacrtovane kilometre!')
+                
+                    else:           
+                        izracunana_cena = round((vnesena_cena_goriva * vneseni_nacrtovani_km) / poraba , 2)
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0, str(izracunana_cena))
+
+                else:
+                    vnesena_cena_goriva = float(self.vnos3.get())
+                    vneseni_nacrtovani_km = float(self.vnos4.get())
+                    
+                    if vnesena_cena_goriva == 0 and vneseni_nacrtovani_km == 0:
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0,'Vnesite vrednosti!')
+                        
+                    elif vnesena_cena_goriva == 0 and vneseni_nacrtovani_km != 0:
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0, 'Vnesite ceno goriva!')
+                        
+                    elif vnesena_cena_goriva != 0 and vneseni_nacrtovani_km == 0:
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0, 'Vnesite nacrtovane kilometre!')
+                
+                    else:           
+                        izracunana_cena = round((vnesena_cena_goriva * vneseni_nacrtovani_km) / poraba , 2)
+                        self.izpis34.delete(0, tk.END)
+                        self.izpis34.insert(0, str(izracunana_cena))
+
+            # Če niso samo števila, vrne 'Neveljaven vnos'.    
+            else:
+                self.izpis34.delete(0, tk.END)
+                self.izpis34.insert(0, 'Neveljaven vnos!')
+
+
+                
 
 
 class GorivoVmesnik:
     def __init__(self):
         #self.gorivo = GorivoModel(self)
+        # ???????.title('Poraba goriva')
         self.okno = tk.Tk()
         
                                        
@@ -113,7 +192,7 @@ class GorivoVmesnik:
         self.izpis34 = tk.Entry(justify = 'center')
 
         self.gumb12 = tk.Button(self.okno, text = '=>', command = self.izracun12)
-        self.gumb34 = tk.Button(self.okno, text = '=>') #command = self.izracun34
+        self.gumb34 = tk.Button(self.okno, text = '=>', command = self.izracun34)
         
         
         self.naslov.grid(row = 0, column = 0)
@@ -143,11 +222,18 @@ class GorivoVmesnik:
         self.gumb34.grid(row = 5, column = 11)
 
         self.okno.mainloop()
-          
 
+
+    # Fukcija izračuna porabo na 4 dec. mesta iz količine goriva ter prevoženih km.          
     def izracun12(self):
         self.gorivo = GorivoModel(self)
         self.gorivo.izracun12()
+
+
+    # Funkcija izračuna ceno načrtovane poti na 2 dec. mesti iz cene goriva, načrtovanih km te iz že prej izračunane porabe
+    def izracun34(self):
+        self.gorivo = GorivoModel(self)
+        self.gorivo.izracun34()
 
 
    
