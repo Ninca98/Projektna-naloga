84692f1d4465672e548c23921344be4adb9e5b64
Ninca98 <strovs.nina@gmail.com>
2018-08-07 23:06:52 +0200

    Dodana 'zgodovina vnosov', urejeni funkciji, urejeni razmiki med okenci.

--- Poraba goriva.py
+++ Poraba goriva.py
@@ -1,7 +1,7 @@
 # PORABA GORIVA #
 
 import tkinter as tk
-from tkinter.font import Font
+# from tkinter.font import Font
 from tkinter import *
 
 
@@ -41,48 +41,41 @@ class GorivoModel:
         self.izpis34 = vmesnik.izpis34
 
 
-    def izracun12(self):
+    def izracun12(self, prvi_vnos, drugi_vnos):
         izracunana_poraba = 0
 
         # Najprej preverimo, ali so v vnosnih poljih samo števila.
-        if ali_so_samo_stevila(self.vnos1.get()) and ali_so_samo_stevila(self.vnos2.get()):
-            # Če je napisana dec. vejica, jo zamenjamo s piko.
-            vnos1 = self.vnos1.get().replace(',', '.')
-            vnos2 = self.vnos2.get().replace(',', '.')
-            vnesena_kolicina_goriva = float(vnos1)
-            vneseni_km = float(vnos2)
+        if ali_so_samo_stevila(prvi_vnos) and ali_so_samo_stevila(drugi_vnos):
+            # Če je napisana dec. vejica, jo zamenjamo s piko.          
+            vnesena_kolicina_goriva = float(prvi_vnos.replace(',', '.'))
+            vneseni_km = float(drugi_vnos.replace(',', '.'))
         
             if vnesena_kolicina_goriva == 0 and vneseni_km == 0:
-                self.izpis12.delete(0, tk.END)
-                self.izpis12.insert(0,'Vnesite vrednosti!')
+                return 'Vnesite vrednosti!'
                 
             elif vnesena_kolicina_goriva == 0 and vneseni_km != 0:
-                self.izpis12.delete(0, tk.END)
-                self.izpis12.insert(0, 'Vnesite količino goriva!')
+                return 'Vnesite količino goriva!'
                 
             elif vnesena_kolicina_goriva != 0 and vneseni_km == 0:
-                self.izpis12.delete(0, tk.END)
-                self.izpis12.insert(0, 'Vnesite kilometre!')
+               return 'Vnesite kilometre!'
         
             else:           
                 izracunana_poraba = round(vneseni_km / vnesena_kolicina_goriva , 4)
-                self.izpis12.delete(0, tk.END)
-                self.izpis12.insert(0, str(izracunana_poraba))
+                return str(izracunana_poraba)
+
 
         # Če niso samo števila, funkcija vrne 'Neveljaven vnos'.        
         else:
-            self.izpis12.delete(0, tk.END)
-            self.izpis12.insert(0, 'Neveljaven vnos!')
+            return 'Neveljaven vnos!'
 
 
 
-    def izracun34(self):
+    def izracun34(self, poraba, tretji_vnos, cetrti_vnos):
         izracunana_cena = 0
 
         # Najprej preverimo, če je poraba sploh izračunana.
-        if self.izpis12.get() in ['Vnesite vrednosti!', 'Vnesite količino goriva!', 'Vnesite kilometre!', 'Neveljaven vnos!']:
-            self.izpis34.delete(0, tk.END)
-            self.izpis34.insert(0, 'Izračunajte porabo!')
+        if poraba in ['Vnesite vrednosti!', 'Vnesite količino goriva!', 'Vnesite kilometre!', 'Neveljaven vnos!']:
+            return 'Izračunajte porabo!'
 
 ##        # Preverimo, če je poraba slučajno 0.
 ##        elif self.izpis12.get() == 0:
@@ -91,35 +84,28 @@ class GorivoModel:
 
         # V primeru, da je poraba izračunana, nadaljujemo s funkcijo.
         else:
-            poraba = float(self.izpis12.get())
-            if ali_so_samo_stevila(self.vnos3.get()) and ali_so_samo_stevila(self.vnos4.get()):  
-                vnos3 = self.vnos3.get().replace(',', '.')
-                vnos4 = self.vnos4.get().replace(',', '.')
-                vnesena_cena_goriva = float(vnos3)
-                vneseni_nacrtovani_km = float(vnos4)
+            stevilska_poraba = float(poraba)
+            if ali_so_samo_stevila(tretji_vnos) and ali_so_samo_stevila(cetrti_vnos):  
+                vnesena_cena_goriva = float(tretji_vnos.replace(',', '.'))
+                vneseni_nacrtovani_km = float(cetrti_vnos.replace(',', '.'))
             
         
                 if vnesena_cena_goriva == 0 and vneseni_nacrtovani_km == 0:
-                    self.izpis34.delete(0, tk.END)
-                    self.izpis34.insert(0,'Vnesite vrednosti!')
+                    return'Vnesite vrednosti!'
                     
                 elif vnesena_cena_goriva == 0 and vneseni_nacrtovani_km != 0:
-                    self.izpis34.delete(0, tk.END)
-                    self.izpis34.insert(0, 'Vnesite ceno goriva!')
+                    return 'Vnesite ceno goriva!'
                     
                 elif vnesena_cena_goriva != 0 and vneseni_nacrtovani_km == 0:
-                    self.izpis34.delete(0, tk.END)
-                    self.izpis34.insert(0, 'Vnesite načrtovane km!')
+                    return 'Vnesite načrtovane km!'
             
                 else:           
-                    izracunana_cena = round((vnesena_cena_goriva * vneseni_nacrtovani_km) / poraba , 2)
-                    self.izpis34.delete(0, tk.END)
-                    self.izpis34.insert(0, str(izracunana_cena))
+                    izracunana_cena = round((vnesena_cena_goriva * vneseni_nacrtovani_km) / stevilska_poraba , 2)
+                    return str(izracunana_cena)
 
             # Če niso samo števila, vrne 'Neveljaven vnos'.    
             else:
-                self.izpis34.delete(0, tk.END)
-                self.izpis34.insert(0, 'Neveljaven vnos!')
+                return 'Neveljaven vnos!'
 
 
                 
@@ -129,70 +115,73 @@ class GorivoVmesnik:
     def __init__(self):
         self.okno = tk.Tk()
         self.okno.title('Poraba goriva')
-        self.okvir12 = tk.Frame()
-        #self.okvir34 = tk.Frame()
-        #self.okvir_skupen = tk.Frame()
+        self.okvir_vnos = tk.Frame()
         self.okvir_zgo = tk.Frame()
 
-        self.okvir12.pack()#side = LEFT)
-        #self.okvir34.pack(side = RIGHT)
-        #self.okvir_skupen.pack()
+        self.okvir_vnos.pack()
         self.okvir_zgo.pack(side = BOTTOM)
         
                                        
-        self.naslov1 = tk.Label(self.okvir12, text = 'Količina goriva')
-        self.naslov2 = tk.Label(self.okvir12, text = 'Prevoženi km')
-        self.naslov3 = tk.Label(self.okvir12, text = 'Cena goriva')
-        self.naslov4 = tk.Label(self.okvir12, text = 'Načrtovani km')
-        self.naslov12 = tk.Label(self.okvir12, text = 'Poraba')
-        self.naslov34 = tk.Label(self.okvir12, text = 'Cena vožnje')
-        self.naslov_zgo = tk.Label(self.okvir_zgo, text = 'Zgodovina vnosov') #justify = 'left'
+        self.naslov1 = tk.Label(self.okvir_vnos, text = 'Količina goriva')
+        self.naslov2 = tk.Label(self.okvir_vnos, text = 'Prevoženi km')
+        self.naslov3 = tk.Label(self.okvir_vnos, text = 'Cena goriva')
+        self.naslov4 = tk.Label(self.okvir_vnos, text = 'Načrtovani km')
+        self.naslov12 = tk.Label(self.okvir_vnos, text = 'Poraba')
+        self.naslov34 = tk.Label(self.okvir_vnos, text = 'Cena vožnje')
+        self.naslov_zgo = tk.Label(self.okvir_zgo, text = 'Zgodovina vnosov')
 
-        self.enota1 = tk.Label(self.okvir12, text = 'l')
-        self.enota2 = tk.Label(self.okvir12, text = 'km')
-        self.enota3 = tk.Label(self.okvir12, text = '€/l')
-        self.enota4 = tk.Label(self.okvir12, text = 'km')
-        self.enota12 = tk.Label(self.okvir12, text = 'km/l')
-        self.enota34 = tk.Label(self.okvir12, text = '€')
+        self.enota1 = tk.Label(self.okvir_vnos, text = 'l')
+        self.enota2 = tk.Label(self.okvir_vnos, text = 'km')
+        self.enota3 = tk.Label(self.okvir_vnos, text = '€/l')
+        self.enota4 = tk.Label(self.okvir_vnos, text = 'km')
+        self.enota12 = tk.Label(self.okvir_vnos, text = 'km/l')
+        self.enota34 = tk.Label(self.okvir_vnos, text = '€')
        
-        self.vnos1 = tk.Entry(self.okvir12, justify = 'center', bd = 3, width = 18)
-        self.vnos2 = tk.Entry(self.okvir12, justify = 'center', bd = 3, width = 18)
-        self.vnos3 = tk.Entry(self.okvir12, justify = 'center', bd = 3, width = 18)
-        self.vnos4 = tk.Entry(self.okvir12, justify = 'center', bd = 3, width = 18)
+        self.vnos1 = tk.Entry(self.okvir_vnos, justify = 'center', bd = 3, width = 18)
+        self.vnos2 = tk.Entry(self.okvir_vnos, justify = 'center', bd = 3, width = 18)
+        self.vnos3 = tk.Entry(self.okvir_vnos, justify = 'center', bd = 3, width = 18)
+        self.vnos4 = tk.Entry(self.okvir_vnos, justify = 'center', bd = 3, width = 18)
         
-        self.izpis12 = tk.Entry(self.okvir12, justify = 'center', bd = 5, width = 25)
-        self.izpis34 = tk.Entry(self.okvir12, justify = 'center', bd = 5, width = 25)
+        self.izpis12 = tk.Entry(self.okvir_vnos, justify = 'center', bd = 5, width = 25)
+        self.izpis34 = tk.Entry(self.okvir_vnos, justify = 'center', bd = 5, width = 25)
 
-        self.gumb12 = tk.Button(self.okvir12, text = '=>', command = self.izracun12, bd = 3, width = 4)
-        self.gumb34 = tk.Button(self.okvir12, text = '=>', command = self.izracun34, bd = 3, width = 4)
+        self.gumb12 = tk.Button(self.okvir_vnos, text = '=>', command = self.izracun12, bd = 3, width = 4)
+        self.gumb34 = tk.Button(self.okvir_vnos, text = '=>', command = self.izracun34, bd = 3, width = 4)
         
-
        
-        self.naslov1.grid(row = 2, column = 0)
-        self.naslov2.grid(row = 6, column = 0)
-        self.naslov3.grid(row = 2, column = 8)
-        self.naslov4.grid(row = 6, column = 8)
-        self.naslov12.grid(row = 4, column = 5)
-        self.naslov34.grid(row = 4, column = 13)
-        self.naslov_zgo.grid(row = 8, column = 0)
+        self.naslov1.grid(row = 1, column = 1)
+        self.naslov2.grid(row = 5, column = 1)
+        self.naslov3.grid(row = 1, column = 9)
+        self.naslov4.grid(row = 5, column = 9)
+        self.naslov12.grid(row = 3, column = 6)
+        self.naslov34.grid(row = 3, column = 14)
+        self.naslov_zgo.pack()
 
-        self.enota1.grid(row = 3, column = 1)
-        self.enota2.grid(row = 7, column = 1)
-        self.enota3.grid(row = 3, column = 9)
-        self.enota4.grid(row = 7, column = 9)
-        self.enota12.grid(row = 5, column = 6)
-        self.enota34.grid(row = 5, column = 14)
+        self.enota1.grid(row = 2, column = 2)
+        self.enota2.grid(row = 6, column = 2)
+        self.enota3.grid(row = 2, column =10)
+        self.enota4.grid(row = 6, column = 10)
+        self.enota12.grid(row = 4, column = 7)
+        self.enota34.grid(row = 4, column = 15)
                        
-        self.vnos1.grid(row = 3, column = 0)
-        self.vnos2.grid(row = 7, column = 0)
-        self.vnos3.grid(row = 3, column = 8)
-        self.vnos4.grid(row = 7, column = 8)
+        self.vnos1.grid(row = 2, column = 1)
+        self.vnos2.grid(row = 6, column = 1)
+        self.vnos3.grid(row = 2, column = 9)
+        self.vnos4.grid(row = 6, column = 9)
 
-        self.izpis12.grid(row = 5, column = 5)
-        self.izpis34.grid(row = 5, column =13)
+        self.izpis12.grid(row = 4, column = 6)
+        self.izpis34.grid(row = 4, column =14)
 
-        self.gumb12.grid(row = 5, column = 3)
-        self.gumb34.grid(row = 5, column = 11)
+        self.gumb12.grid(row = 4, column = 4)
+        self.gumb34.grid(row = 4, column = 12)
+
+        self.okvir_vnos.grid_columnconfigure(0, minsize = 10)
+        self.okvir_vnos.grid_columnconfigure(5, minsize = 5)
+        self.okvir_vnos.grid_columnconfigure(8, minsize = 80)
+        self.okvir_vnos.grid_columnconfigure(13, minsize = 5)
+        self.okvir_vnos.grid_columnconfigure(15, minsize = 20)
+        self.okvir_vnos.grid_rowconfigure(0, minsize = 10)
+        self.okvir_vnos.grid_rowconfigure(7, minsize = 25)
 
 
         self.gorivo = GorivoModel(self)
@@ -201,12 +190,25 @@ class GorivoVmesnik:
 
     # Fukcija izračuna porabo na 4 dec. mesta iz količine goriva ter prevoženih km.          
     def izracun12(self):
-        self.gorivo.izracun12()
+        rezultat12 = self.gorivo.izracun12(self.vnos1.get(), self.vnos2.get())
+        self.izpis12.delete(0, tk.END)
+        self.izpis12.insert(0, rezultat12)
+
+        if ali_so_samo_stevila(rezultat12):
+            self.izpis_zgo = tk.Label(self.okvir_zgo, text='Količina goriva: {0} l,   prevoženi km: {1} km,   poraba: {2} km/l.'.format(self.vnos1.get(), self.vnos2.get(), self.izpis12.get()))
+            self.izpis_zgo.pack()
+            
 
 
     # Funkcija izračuna ceno načrtovane poti na 2 dec. mesti iz cene goriva, načrtovanih km te iz že prej izračunane porabe
     def izracun34(self):
-        self.gorivo.izracun34()
+        rezultat34 = self.gorivo.izracun34(self.izpis12.get(), self.vnos3.get(), self.vnos4.get())
+        self.izpis34.delete(0, tk.END)
+        self.izpis34.insert(0, rezultat34)
+
+        if ali_so_samo_stevila(rezultat34):
+            self.izpis_zgo = tk.Label(self.okvir_zgo, text='Poraba: {0} km/l,   cena goriva: {1} €,   načrtovani km: {2} km,   cena vožnje: {3} €.'.format(self.izpis12.get(), self.vnos3.get(), self.vnos4.get(), self.izpis34.get()))
+            self.izpis_zgo.pack()
 
 
    
